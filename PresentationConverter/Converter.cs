727849f52adcb6008d69ab8dd80a3fd399dc2171
Denis Oleynik <denis1riddle@gmail.com>
2023-05-25 00:17:33 +0300

    Added a package for working with presentations

---
+++ PresentationConverter/Converter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Office.Interop.PowerPoint;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace PresentationConverter
+{
+    internal class Converter
+    {
+
+    }
+}
