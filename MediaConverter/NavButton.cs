7e7da69f63741f7f390451077c5e5449be8d60b7
Denis Oleynik <denis1riddle@gmail.com>
2023-06-05 23:08:26 +0300

    Added main WPF window w\ sidebar

---
+++ MediaConverter/NavButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MediaConverter
+{
+    public class NavButton : ListBoxItem
+    {
+        static NavButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavButton), new FrameworkPropertyMetadata(typeof(NavButton)));
+        }
+
+        public Uri Navlink
+        {
+            get { return (Uri)GetValue(NavlinkProperty); }
+            set { SetValue(NavlinkProperty, value); }
+        }
+
+        public static readonly DependencyProperty NavlinkProperty = DependencyProperty.Register("Navlink", typeof(Uri), typeof(NavButton), new PropertyMetadata(null));
+
+        public ImageSource Icon
+        {
+            get { return (ImageSource)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(ImageSource), typeof(NavButton), new PropertyMetadata(null));
+    }
+}
