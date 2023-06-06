bc36490379f9c3a26fd3ccaf8e448042dffa7139
Denis Oleynik <denis1riddle@gmail.com>
2023-06-06 14:01:08 +0300

    Implemented select path page

--- MediaConverter/Pages/ApplicationPathSelectorPage.xaml.cs
+++ MediaConverter/Pages/ApplicationPathSelectorPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System;
 
 namespace MediaConverter.Pages
 {
@@ -20,9 +11,29 @@ namespace MediaConverter.Pages
     /// </summary>
     public partial class ApplicationPathSelectorPage : Page
     {
+        private string _selectedPath = string.Empty;
+
         public ApplicationPathSelectorPage()
         {
             InitializeComponent();
+        }
+
+        private void ButtonSelectPath_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Решение (*.sln)|*.sln";
+            dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog() == true)
+            {
+                _selectedPath = dialog.FileName;
+                TextBoxSelectedPath.Text = _selectedPath;
+            }
+        }
+
+        private void ButtonSubmitPath_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
